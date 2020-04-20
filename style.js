a6c81687c9e88fd7d3a47cb6cca8dcb920ea4d9b
Modocero <34664022+Modocero@users.noreply.github.com>
2020-04-19 21:37:59 -0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,7 @@
+$("#chat-box").css("bottom", "20px")
+$("#chat-box").css("fontSize", "13px")
+$("#chat-box").css("border-width","2px");
+$("#chat-box").css("border-color", "red", "important");
+$("#chat-box").css('border-radius', '25px');
+$("#minimap").css('background-repeat': 'no-repeat');
+$("#leaderboard-hud").attr('style','background-image:url("https://i.imgur.com/s8L5ydG.png") !important');
